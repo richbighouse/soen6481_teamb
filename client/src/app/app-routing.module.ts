9f53d463b354e02cbedcd7262a7e033ca611f292
vedanshi <vedanshi.patel11@gmail.com>
2021-10-31 20:25:23 -0400

    starting user view info

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SelfAssessmentComponent } from './components/self-assessment/self-assessment.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 const routes: Routes = [
@@ -27,6 +28,10 @@ const routes: Routes = [
   {
     path: 'self-assessment',
     component: SelfAssessmentComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 

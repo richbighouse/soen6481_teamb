590f1ab5c232bf78fcec61d2cd7084069fca1200
Richard Grand'Maison <rich.grandmaison@gmail.com>
2021-11-13 17:10:51 -0500

    Modify how we show events

--- client/src/app/components/schedule/schedule.component.ts
+++ client/src/app/components/schedule/schedule.component.ts
@@ -11,8 +11,17 @@ export class ScheduleComponent implements OnInit {
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     contentHeight: '75vh',
+    dayMaxEventRows: true, // for all non-TimeGrid views
+    views: {
+      dayGridMonth: {
+        displayEventEnd: true
+      },
+      timeGrid: {
+        dayMaxEventRows: 4 // adjust to 6 only for timeGridWeek/timeGridDay
+      }
+    },
     events: [
-      { title: 'event 1', date: '2021-11-01' },
+      { title: 'event 1', start: '2021-11-01T12:30:00', end: '2021-11-01T14:30:00' },
       { title: 'event 2', date: '2021-11-02' },
       { title: 'event 3', date: '2021-11-02' },
       { title: 'event 4', date: '2021-11-02' },

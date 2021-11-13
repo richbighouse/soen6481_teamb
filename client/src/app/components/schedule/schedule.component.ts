d94b31bfeb2a20ee2559971e08f1d41b099e55d9
Richard Grand'Maison <rich.grandmaison@gmail.com>
2021-11-13 16:59:42 -0500

    Init calendar

--- client/src/app/components/schedule/schedule.component.ts
+++ client/src/app/components/schedule/schedule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-schedule',
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    contentHeight: '75vh',
+    events: [
+      { title: 'event 1', date: '2021-11-01' },
+      { title: 'event 2', date: '2021-11-02' },
+      { title: 'event 3', date: '2021-11-02' },
+      { title: 'event 4', date: '2021-11-02' },
+      { title: 'event 5', date: '2021-11-02' },
+      { title: 'event 6', date: '2021-11-02' }
+    ],
+    eventClick: function(eventClickInfo ) {
+      console.log(eventClickInfo.event);
+      console.log(eventClickInfo.event.start);
+      console.log(eventClickInfo.event.title);
+    }
+
+
+    
+  };
 
   constructor() { }
 

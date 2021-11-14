548847f534b83460f8d04c57e9adcbbc8d580d80
Richard Grand'Maison <rich.grandmaison@gmail.com>
2021-11-14 11:19:08 -0500

    Add more data to the generated schema

--- database/models/schema_v004.sql
+++ database/models/schema_v004.sql
@@ -67,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `covid`.`assessment` (
   `date` DATE NOT NULL,
   `viewedByNurse` TINYINT NOT NULL,
   `fkPatientId` INT NOT NULL,
-  `q_difficultyBreathing` VARCHAR(45) NOT NULL,
+  `q_difficultyBreathing` TINYINT NOT NULL,
   `q_ageRange` ENUM('', '5', '6-17', '18+') NULL,
   `q_firstSymptoms` TINYINT NULL,
   `q_situation` TINYINT NULL,
@@ -137,10 +137,52 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `covid`;
-INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (DEFAULT, 'Patient Test', '123 Test', '2000-01-01', '5147557112', 'patient@test.com', 'patient', 1, '2021-10-17', NULL, 1, 1, NULL);
-INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (DEFAULT, 'Nurse Test', '123 Test', '2002-02-02', '5147557112', 'nurse@test.com', 'nurse', 3, '2021-10-17', NULL, 1, 1, '12345678');
-INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (DEFAULT, 'Doctor Test', '123 Test', '2003-03-03', '5147557112', 'doctor@test.com', 'doctor', 2, '2021-10-17', NULL, 1, 1, '987654321');
-INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (DEFAULT, 'Admin', '1234 Admin', '2004-04-04', '5147557112', 'admin@test.com', 'admin', 4, '2021-10-17', NULL, 1, 1, NULL);
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (1, 'Patrick Smith', '123 Test', '2000-01-01', '5147557112', 'patient@test.com', 'patient', 1, '2021-10-17', NULL, 1, 1, NULL);
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (2, 'Naril Logan', '123 Test', '2002-02-02', '5147557112', 'nurse@test.com', 'nurse', 3, '2021-10-17', NULL, 1, 1, '12345678');
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (3, 'Daniel Chang', '123 Test', '2003-03-03', '5147557112', 'doctor@test.com', 'doctor', 2, '2021-10-17', NULL, 1, 1, '987654321');
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (4, 'Admin', '1234 Admin', '2004-04-04', '5147557112', 'admin@test.com', 'admin', 4, '2021-10-17', NULL, 1, 1, NULL);
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (5, 'Paul Miller', '222 Avenue', '1999-01-01', '4507557777', 'pmiller@test.com', 'pmiller', 1, '2021-11-11', NULL, 1, 1, NULL);
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (6, 'Peppa Swine', '333 Street', '2000-02-02', '8195857766', 'pswine@test.com', 'pswine', 1, '2021-11-11', NULL, 1, 1, NULL);
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (7, 'Phil Gamon', '444 Road', '2001-03-03', '5147310607', 'pgamon@test.com', 'pgamon', 1, '2021-11-11', NULL, 1, 1, NULL);
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (8, 'Phillys Vance', '555 Boulevard', '1975-02-04', '5147331234', 'pvance@test.com', 'pvance', 1, '2021-11-11', NULL, 1, 1, NULL);
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (9, 'Nancy Brown', '666 Highway', '1985-05-15', '5148989909', 'nbrown@test.com', 'nbrown', 3, '2021-11-11', NULL, 1, 0, '335577');
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (10, 'Nicolas Tesla', '777 Crescent', '1956-11-11', '5142546011', 'ntesla@test.com', 'ntesla', 3, '2021-11-11', NULL, 1, 1, '556677');
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (11, 'Daryl Phibin', '888 Rondpoint', '1988-05-02', '5144441919', 'dphibin@test.com', 'dphibin', 2, '2021-11-11', NULL, 1, 1, '112233');
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (12, 'Dom Khan', '999 Ruelle', '1995-11-12', '5143546676', 'dkhan@test.com', 'dkhan', 2, '2021-11-11', NULL, 1, 0, '667788');
+INSERT INTO `covid`.`user` (`id`, `fullName`, `address`, `dateOfBirth`, `phoneNumber`, `email`, `password`, `fkUserType`, `registrationDate`, `lastLoginDate`, `active`, `approved`, `registrationNumber`) VALUES (13, 'Prince Consuella', '1001 Castle', '2002-07-07', '5145695808', 'pconsuella@test.com', 'pconsuella', 1, '2021-11-11', NULL, 1, 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `covid`.`assessment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `covid`;
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (1, '2021-11-01', 0, 1, 1, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (2, '2021-11-01', 0, 1, 1, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (3, '2021-11-02', 0, 1, 1, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (4, '2021-11-03', 0, 1, 1, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (5, '2021-11-03', 0, 1, 1, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (6, '2021-11-04', 0, 1, 1, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (7, '2021-11-05', 0, 6, 0, '5', 1, 0, NULL, 1, 1, 1, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (8, '2021-11-06', 0, 7, 0, '6-17', 0, 0, 1, 0, 0, 0, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (9, '2021-11-07', 0, 8, 0, '18+', 1, NULL, NULL, 0, 0, 0, NULL, DEFAULT);
+INSERT INTO `covid`.`assessment` (`id`, `date`, `viewedByNurse`, `fkPatientId`, `q_difficultyBreathing`, `q_ageRange`, `q_firstSymptoms`, `q_situation`, `q_secondSymptoms`, `q_hasBeenCloseContact`, `q_hasBeenTested`, `q_hasTraveled`, `assignedDoctorId`, `rejected`) VALUES (10, '2021-11-08', 0, 9, 0, '18+', 0, 1, NULL, 0, 1, 0, NULL, DEFAULT);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `covid`.`appointment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `covid`;
+INSERT INTO `covid`.`appointment` (`id`, `location`, `dateTime`, `fkPatientId`, `fkProfessionalId`) VALUES (1, 'Hospital', '2021-11-25 12:00:00', 1, 11);
+INSERT INTO `covid`.`appointment` (`id`, `location`, `dateTime`, `fkPatientId`, `fkProfessionalId`) VALUES (2, 'Hospital', '2021-11-25 14:00:00', 6, 11);
+INSERT INTO `covid`.`appointment` (`id`, `location`, `dateTime`, `fkPatientId`, `fkProfessionalId`) VALUES (3, 'Hospital', '2021-11-25 08:00:00', 7, 11);
+INSERT INTO `covid`.`appointment` (`id`, `location`, `dateTime`, `fkPatientId`, `fkProfessionalId`) VALUES (4, 'Hospital', '2021-11-17 09:30:00', 8, 11);
+INSERT INTO `covid`.`appointment` (`id`, `location`, `dateTime`, `fkPatientId`, `fkProfessionalId`) VALUES (5, 'Hospital', '2021-12-01 10:30:00', 9, 11);
 
 COMMIT;
 

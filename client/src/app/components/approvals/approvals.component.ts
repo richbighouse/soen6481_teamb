bb014b42231a85223d62cedf027abd2c155a1f50
Richard Grand'Maison <rich.grandmaison@gmail.com>
2021-11-25 18:52:16 -0500

    Add confirmation before approving or rejecting a user

--- client/src/app/components/approvals/approvals.component.ts
+++ client/src/app/components/approvals/approvals.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTable } from '@angular/material/table';
 import { User } from 'shared/models/models';
 import { ApprovalService } from 'src/app/services/approval.service';
+import { DialogConfirmComponent } from '../dialog-confirm/dialog-confirm.component';
 
 @Component({
   selector: 'app-approvals',
@@ -16,6 +19,8 @@ export class ApprovalsComponent implements OnInit {
 
   constructor(
     private approvalService: ApprovalService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
@@ -42,35 +47,53 @@ export class ApprovalsComponent implements OnInit {
   }
 
   onApproval(user:User) {
-    this.approvalService.postApproveStatus(user).subscribe(
-      response => {
-        console.log("done")    
-      }  
-    )
-    this.refreshRows(user);
+    const dialogRef = this.dialog.open(DialogConfirmComponent, {
+      data: {
+        question: "Do you really want to approve this user's registration?"
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (res.isConfirm === true) {
+        this.approvalService.postApproveStatus(user).subscribe(
+          response => {
+            console.log("done")    
+          }  
+        )
+        this.refreshRows(user)
+      }
+    })
   }
 
   refreshRows(user:User)
   {
     for (let i = 0; i < this.dataSource.length; i++) 
     {
-
-             if(this.dataSource[i].email == user.email)
-             {
-               console.log(this.dataSource[i].email)
-               this.dataSource.splice(i, 1);
-             }
+      if(this.dataSource[i].email == user.email) {
+        console.log(this.dataSource[i].email)
+        this.dataSource.splice(i, 1);
+      }
     }
     this.table.renderRows();
   }
 
   onRejection(user:User) {
-    this.approvalService.postRejectStatus(user).subscribe(
-      response => {
-        console.log("done")     
+    const dialogRef = this.dialog.open(DialogConfirmComponent, {
+      data: {
+        question: "Do you really want to reject this user's registration?"
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      if (res.isConfirm === true) {
+        this.approvalService.postRejectStatus(user).subscribe(
+          response => {
+            console.log("done")     
+          }
+        )
+        this.refreshRows(user);
       }
-    )
-    this.refreshRows(user);
+    })
   }
 
   getUserType(userType: number)

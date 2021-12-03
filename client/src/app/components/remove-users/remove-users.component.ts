271f08cd9e50953a74443069bcd76977d1567eea
Richard Grand'Maison <rich.grandmaison@gmail.com>
2021-12-02 21:44:20 -0500

    Remove admins from list of removable users, and fix sort

--- client/src/app/components/remove-users/remove-users.component.ts
+++ client/src/app/components/remove-users/remove-users.component.ts
@@ -30,7 +30,8 @@ export class RemoveUsersComponent implements OnInit, AfterViewInit{
     (res => {
       console.log(res);
       console.log("testing now");
-      this.dataSource.data = res;
+      const nonAdminUsers = res.filter(user => user.fkUserType !== 4)
+      this.dataSource.data = nonAdminUsers;
     },
     err => {
       console.log(err);
@@ -53,7 +54,8 @@ export class RemoveUsersComponent implements OnInit, AfterViewInit{
     (res => {
       console.log(res);
       console.log("testing now");
-      this.dataSource.data = res;
+      const nonAdminUsers = res.filter(user => user.fkUserType !== 4)
+      this.dataSource.data = nonAdminUsers;
     },
     err => {
       console.log(err);
@@ -85,9 +87,30 @@ export class RemoveUsersComponent implements OnInit, AfterViewInit{
       return 'Nurse'
     } else if (fkUserType === 2) {
       return 'Doctor'
-    } else {
+    } else if (fkUserType === 1) {
       return 'Patient'
+    } else {
+      return 'Admin'
     }
   }
 
+  sortData(sort: Sort) {
+    console.log(sort);
+
+    this.dataSource.data = this.dataSource.data.sort((a: User, b : User) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'id': return this.compare(a.id, b.id, isAsc);
+        case 'email': return this.compare(a.email, b.email, isAsc);
+        case 'fullName': return this.compare(a.fullName, b.fullName, isAsc);
+        case 'registrationNumber': return this.compare(a.registrationNumber, b.registrationNumber, isAsc);
+        case 'fkUserType': return this.compare(a.fkUserType, b.fkUserType, isAsc);
+        default: return 0;
+      }
+    });
+  }
+
+  compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
 }
